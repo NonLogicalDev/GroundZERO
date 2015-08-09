a72b33e0314f0d858913fc25578c5ce8424b8eb9
NonLogicalDev <utkin.oleg@me.com>
2015-08-09 14:31:27 -0700

    Updating the default captured screenshot location

--- configs/mac/scripts/set-up-screencapture.sh
+++ configs/mac/scripts/set-up-screencapture.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+SCREENSHOT_FOLDER="${HOME}/Pictures/Screenshots"
+
+# Make sure the folder exists
+mkdir -p $SCREENSHOT_FOLDER
+
 # Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+defaults write com.apple.screencapture location -string "$SCREENSHOT_FOLDER"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
