4a0e47f050b37205c596c9e264551240403df2ef
Oleg Utkin <dev0@nonlogical.io>
2016-09-12 00:24:50 -0700

    Update

--- configs/mac/scripts/osx-superpowers.sh
+++ configs/mac/scripts/osx-superpowers.sh
@@ -405,9 +405,8 @@ defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool t
 ###############################################################################
 
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-	"Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" \
-	"Transmission" "Twitter" "iCal"; do
+	"Dock" "Finder" "Mail" "Messages" "SizeUp" "Spectacle" "SystemUIServer" \
+	"Twitter" "iCal"; do
 	killall "${app}" > /dev/null 2>&1
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
