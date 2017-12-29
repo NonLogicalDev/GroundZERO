2a9864d818aedcf31059931c1285326e55032b84
Oleg Utkin <oleg.utkin@uber.com>
2017-12-28 16:40:43 -0800

    Update

--- scripts/deploy/bootstrap.sh
+++ scripts/deploy/bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
+set -x
+
 GZ_LOC="$HOME/.groundzero_test"
 BOOTSTRAP_READY="no"
 
-function get_platform {
+function get_platform() {
   if [[ "$(uname)" == "Darwin" ]]; then
     echo "MAC"
   elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
@@ -14,31 +17,49 @@ function get_platform {
   fi
 }
 
+function install_cl_tools() {
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" -v;
+}
+
+function install_homebrew() {
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+}
+
+function install_ansible() {
+  brew update;
+  brew install ansible;
+}
+
 if [[ "$(get_platform)" == "MAC" ]]; then
   # Install developer tools
-  if [[ $(/usr/bin/gcc 2>&1) =~ "no developer tools were found" ]] || [[ ! -x /usr/bin/gcc ]]; then
-    echo "Info   | Install   | xcode"
-    xcode-select --install
+  if ! $(pkgutil --pkgs | grep "CLTools" -q) ; then
+    echo "Info   | Install   | xcode";
+    install_cl_tools;
   fi
 
   # Download and install Homebrew
   if [[ ! -x /usr/local/bin/brew ]]; then
-    echo "Info   | Install   | homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Info   | Install   | homebrew";
+    install_homebrew;
   fi
 
   # Download and install Ansible
   if [[ ! -x /usr/local/bin/ansible ]]; then
-    echo "Info   | Install   | Ansible"
-    brew update
-    brew install ansible
+    echo "Info   | Install   | Ansible";
+    install_ansible;
   fi
 
   BOOTSTRAP_READY="yes"
 fi
 
 if [[ $BOOTSTRAP_READY == "yes" ]]; then
-  git clone https://github.com/NonLogicalDev/GroundZERO $GZ_LOC
+  git clone https://github.com/NonLogicalDev/GroundZERO $GZ_LOC;
 else
   echo "Error  | Bootstrap Failed" > &2
 fi
