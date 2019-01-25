745403443cd72506a301becc22876f852fd8dbba
Oleg Utkin <oleg.utkin@uber.com>
2019-01-25 09:58:33 -0800

    Switch to using dotter as opposed to legacy perl script

--- dot.sh
+++ dot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR > /dev/null
-  scripts/dot/dotninja $*
+#   scripts/dot/dotninja $*
+  env/bin/dotter "$@"
 popd > /dev/null
